2b9a350fb3bdcde90693aa87a32a2b3dea691088
Paola Ortiz <ortiz.triguis@gmail.com>
2022-02-16 22:30:20 -0300

    Se agrego el pedido a la API

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const pokemonContainer = document.querySelector('.pokemon-container')
 
+function bringPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(response => response.json())
+    .then(data => {
+        createPokemon(data);
+    })
+
+}
+
+function bringPokemons(number){
+    for (let i = 1; i <= number; i++) {
+        bringPokemon(i);
+    }
+}
+
+function createPokemon(pokemon) {
+    const card = document.createElement('div');
+    card.classList.add('bloque');
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('img-container');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement('p');
+    name.classList.add('name');
+    name.textContent = pokemon.name
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+}
+
+bringPokemons(898);
